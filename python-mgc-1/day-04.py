29b7092fd3ea795391180db0c0df45b5ffbe2f59
inhalin <inhalin.kim@gmail.com>
2020-12-30 00:09:17 +0900

    day4 operator

---
+++ python-mgc-1/day-04.py
@@ -0,0 +1,18 @@
+# 두 수 a와 b를 선언한 뒤, a와 b를 이용한 사칙연산 계산기를 만들어 보세요.
+
+print("Simple Calculator")
+
+a=int(input("첫번째 정수 a를 입력하세요 > "))
+b=int(input("두번째 정수 b를 입력하세요 > "))
+
+print("a + b =", a+b)
+print("a - b =", a-b)
+print("a * b =", a*b)
+print("a / b =", a/b)
+print("a // b =", a//b)
+print("a % b =", a%b)
+
+# 두 수 a와 b를 선언한 뒤, a의 b제곱의 값을 출력해보세요.
+
+print("a^b = ", a**b)
+
