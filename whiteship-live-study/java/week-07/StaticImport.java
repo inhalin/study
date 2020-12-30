77d104f0a06c6dd995d0159a12f1e4e180f07ff5
inhalin <inhalin.kim@gmail.com>
2020-12-30 21:02:54 +0900

    w7 package, import, classpath, access modifier

---
+++ whiteship-live-study/java/week-07/StaticImport.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.random;
+import static java.lang.System.out;
+
+public class StaticImport{
+  public static void main(String[] args) {
+    out.println(random());
+  }
+}
