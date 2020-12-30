a263c1e36e215dc62c7cebaca76968c7d9f41b63
inhalin <inhalin.kim@gmail.com>
2020-12-30 12:28:57 +0900

    d4 simple calculator 1.0

--- python-mgc-1/day-04.py
+++ python-mgc-1/day-04.py
@@ -1,7 +1,8 @@
 # 두 수 a와 b를 선언한 뒤, a와 b를 이용한 사칙연산 계산기를 만들어 보세요.
 
-print("Simple Calculator")
+print("<< Simple Calculator 1.0 >>")
 
+print("1. 기본 사칙연산")
 a=int(input("첫번째 정수 a를 입력하세요 > "))
 b=int(input("두번째 정수 b를 입력하세요 > "))
 
@@ -14,5 +15,8 @@ print("a % b =", a%b)
 
 # 두 수 a와 b를 선언한 뒤, a의 b제곱의 값을 출력해보세요.
 
-print("a^b = ", a**b)
+print("2. c의 d제곱")
+c=int(input("첫번째 정수 c를 입력하세요 > "))
+d=int(input("두번째 정수 d를 입력하세요 > "))
 
+print("c^d = ", c**d)
