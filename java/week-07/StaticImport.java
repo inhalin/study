cb1cad58f234598cec5f21fae64bfde1886d9838
inhalin <inhalin.kim@gmail.com>
2021-01-19 21:29:01 +0900

    w10 Thread 클래스와 Runnable 인터페이스, 쓰레드의 상태

--- java/week-07/StaticImport.java
+++
@@ -1,8 +0,0 @@
-import static java.lang.Math.random;
-import static java.lang.System.out;
-
-public class StaticImport{
-  public static void main(String[] args) {
-    out.println(random());
-  }
-}
